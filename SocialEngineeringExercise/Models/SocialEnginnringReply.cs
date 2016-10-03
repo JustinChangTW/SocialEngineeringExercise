5c97689275507f2e050eec7898e212febeb5b12f
justinTW <god.chang.tw@gmail.com>
2016-10-03 14:47:08 +0800

    加入SocialEnginneringReply DataModel

---
+++ SocialEngineeringExercise/Models/SocialEnginnringReply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialEngineeringExercise.Models
+{
+    public class SocialEnginnringReply
+    {
+        public Guid SocialEnginnringGuid { get; set; }
+        public string EmployeeNo { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public DateTime SendDate { get; set; }
+        public string Remark { get; set; }
+        public bool IsClick { get; set; }
+        public DateTime ClickDate { get; set; }
+    }
+}
