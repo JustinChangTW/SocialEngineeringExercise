fe18795575a29316c921b1b0ab51c7fa90f55ca9
justinTW <god.chang.tw@gmail.com>
2016-10-03 16:32:36 +0800

    設定Model說明

--- SocialEngineeringExercise/Models/SocialEnginnringReply.cs
+++ SocialEngineeringExercise/Models/SocialEnginnringReply.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,30 @@ namespace SocialEngineeringExercise.Models
 {
     public class SocialEnginnringReply
     {
+        [DisplayName("序號")]
+        [Description("提供回覆的唯一更新的值")]
         public Guid SocialEnginnringGuid { get; set; }
+
+        [DisplayName("員工編號")]
         public string EmployeeNo { get; set; }
+
+        [DisplayName("員工姓名")]
         public string EmployeeName { get; set; }
+
+        [DisplayName("員工Email")]
         public string EmployeeEmail { get; set; }
+
+        [DisplayName("寄送日期")]
         public DateTime SendDate { get; set; }
+
+        [DisplayName("備註")]
         public string Remark { get; set; }
+
+        [DisplayName("是否被Click")]
+        [Description("提供回覆的唯一更新的值")]
         public bool IsClick { get; set; }
+
+        [DisplayName("被Click日期")]
         public DateTime ClickDate { get; set; }
     }
 }
