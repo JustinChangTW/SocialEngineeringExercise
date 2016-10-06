e7fc0cec18f430ded16b2761a44c0353e3fd11c7
justinTW <god.chang.tw@gmail.com>
2016-10-06 11:18:01 +0800

    調整登錄設定

--- SocialEngineeringExercise/App_Start/IdentityConfig.cs
+++ SocialEngineeringExercise/App_Start/IdentityConfig.cs
@@ -23,16 +23,16 @@ namespace SocialEngineeringExercise
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                //RequireUniqueEmail = true
             };
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                //RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireUppercase = true,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
---
+++ SocialEngineeringExercise/Migrations/201610060100507_連線字串.cs
@@ -0,0 +1,16 @@
+namespace SocialEngineeringExercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class 連線字串 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
